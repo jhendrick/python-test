d74a53d29e67f2d7154439c2c09db3f522100b18
Josh Hendrick <jhendrick@users.noreply.github.com>
2020-02-13 12:26:29 -0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,5 @@
 from rook.serverless import serverless_rook
 
-import rook
-rook.start(token="987cb968bd451072e2fae4ec9a3afa3741261cb58813059d1aa4daf122c9be49", labels={"env":"kubeless"})
-
 @serverless_rook  
 def hello(event, context):
   print event
