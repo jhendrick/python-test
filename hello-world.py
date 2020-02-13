8bf918fff9cfd851362075136070acbc9c4eeb80
Josh Hendrick <jhendrick@users.noreply.github.com>
2020-02-13 14:18:36 -0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,10 @@
 from rook.serverless import serverless_rook
 
+import rook
+
 @serverless_rook  
 def hello(event, context):
-  from rook import auto_start
+  rook.start()
   print event
   rook.flush()
   return event['data']
